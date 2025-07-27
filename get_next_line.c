f7d723079f7cd5bac92d80c9611c7cb7d3364f07
Thawan Silva <thaperei@student.42sp.org.br>
2025-07-27 15:41:30 -0300

    Improv: dealing with two functions calls in sequence at the same fd

--- get_next_line.c
+++ get_next_line.c
@@ -12,36 +12,7 @@
 
 #include "./get_next_line.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	len_dest;
-
-	len_dest = ft_strlen(dest);
-	i = 0;
-	while (i < size && src[i])
-	{
-		dest[len_dest + i] = src[i];
-		i++;
-	}
-	dest[len_dest + i]  = '\0';
-	return (len_dest + i);
-}
-
-int	ft_isnewline(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i++] == '\n')
-			return (1);
-	}
-	return (0);
-}
-
-size_t	ft_get_newline_pos(char *str)
+int	ft_get_newline_pos(char *str)
 {
 	size_t	i;
 
@@ -54,52 +25,62 @@ size_t	ft_get_newline_pos(char *str)
 	return (0);
 }
 
-char	*ft_get_remainder(char *str)
-{
-	size_t	size;
-	char	*remainder;
+//char	*ft_get_remainder(char *str)
+//{
+//	size_t	size;
+//	char	*remainder;
+//
+//	size = ft_strlen(str);
+//	remainder = (char *) malloc(size + 1);
+//	if (!remainder)
+//		return (0);
+//	ft_strlcpy(remainder, str, size);
+//	return (remainder);
+//}
 
-	size = ft_strlen(str);
-	remainder = (char *) malloc(size + 1);
-	if (!remainder)
-		return (0);
-	ft_strlcpy(remainder, str, size);
-	return (remainder);
-}
-
-char	*ft_getline(char *buffer, size_t end)
+char	*ft_parse_line(char *buffer, size_t end)
 {
 	static char	*remainder;
 	char		*str;
 	size_t		buf_len;
 	size_t		rem_len;
 
-	str = NULL;
+	str = (void *)0;
 	buf_len = ft_strlen(buffer);
-	rem_len = 0;
-	if (remainder != (void *)0)
+	rem_len = ft_strlen(remainder);
+	if (buf_len > end) // buffer with \n
 	{
-		rem_len = ft_strlen(remainder);
-		str = (char *) malloc(rem_len + end + 1);
-		if (!str)
-			return ((void *)0);
-		ft_strlcpy(str, remainder, rem_len + 1);
-		ft_strlcpy(str, buffer, end);
-		if (buf_len - end > 0)
+		if (rem_len > 0) // filled remainder
 		{
-			free(remainder);
-			remainder = (char *) malloc(buf_len - end + 1);
-			if (!remainder)
+			str = ft_calloc(rem_len + end, sizeof(char));
+			if (!str)
 				return ((void *)0);
-			ft_strlcpy(remainder, buffer + end + 1, buf_len - end + 1);
+			ft_strlcpy(str, remainder, rem_len);
+			ft_strlcpy(str, buffer, end);
 		}
-	}
-	else
-	{
-		remainder = (char *) malloc(buf_len + 1);
-			if (!remainder)
+		else // empty remainder
+		{
+			str = ft_calloc(buf_len, sizeof(char));
+			if (!str)
 				return ((void *)0);
-		ft_strlcpy(remainder, buffer, buf_len);
+			ft_strlcpy(str, buffer, end);
+		}
+		remainder = ft_calloc(buf_len - end, sizeof(char));
+		ft_strlcpy(remainder, buffer + end + 1, buf_len - end);
+	}
+	else // buffer without \n
+	{
+		if (rem_len > 0 && buf_len == end) // filled remainder
+		{
+			str = ft_strjoin(remainder, buffer);
+			free(remainder);
+			remainder = ft_strjoin(str, "");
+		}
+		else
+		{
+			remainder = ft_calloc(buf_len, sizeof(char));
+			ft_strlcpy(remainder, buffer, buf_len);
+		}
 	}
 	return (str);
 }
@@ -117,16 +98,12 @@ char	*ft_get_next_line(int fd)
 	while (size > 0)
 	{
 		buffer[size] = '\0';
-		if (ft_isnewline(buffer))
-		{
-			// Show buffer until newline and keep the rest
-			end = ft_get_newline_pos(buffer);
-			return (ft_getline(buffer, end));
-		}
+		end = ft_get_newline_pos(buffer);
+		if (end != 0)
+			return (ft_parse_line(buffer, end));
 		else
-			ft_getline(buffer, size);
-		// read next line
+			ft_parse_line(buffer, size);
 		size = read(fd, buffer, BUFFER_SIZE);
 	}
-	return (ft_getline(buffer, end));
+	return (ft_parse_line(buffer, end));
 }
