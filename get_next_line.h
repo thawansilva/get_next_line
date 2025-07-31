0614b597261251ed8c7ac059fb7be4ec82393166
Thawan Silva <thaperei@student.42sp.org.br>
2025-07-31 10:47:37 -0300

    Improv: Organize function and delete unnecessary code

--- get_next_line.h
+++ get_next_line.h
@@ -19,14 +19,11 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-# ifndef OPEN_MAX
-#  define OPEN_MAX 1024
-# endif
-
-size_t	ft_strlen(const char *str);
 char	*get_next_line(int fd);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 #endif 
