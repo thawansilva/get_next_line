e778eab8f32bdb448284632ac526ce8b3f325b58
Thawan Silva <thaperei@student.42sp.org.br>
2025-07-31 10:40:59 -0300

    Improv: Do ft_get_line and ft_get_remainder, fix some norm rules

--- get_next_line.c
+++ get_next_line.c
@@ -21,15 +21,46 @@ static void	*ft_free(char *s1, char *s2)
 
 char	*ft_get_line(char *text)
 {
-	size_t	i;
-	char	*line
+	size_t	line_len;
+	char	*line;
+
+	line = NULL;
+	line_len = 0;
+	while (text[line_len] && text[line_len] != '\n')
+		line_len++;
+	if (!text[line_len])
+		return (text);
+	if (text[line_len] == '\n')
+		line_len++;
+	line = ft_calloc(line_len + 1, sizeof(char));
+	if (!line)
+		return (ft_free(line, NULL));
+	ft_strlcpy(line, text, line_len + 1);
 	return (line);
 }
 
-//char	*ft_get_remainder(char *remainder, char *buffer, size_t end)
-//{
-//}
-//
+char	*ft_get_remainder(char *text)
+{
+	size_t	nl_pos;
+	size_t	rem_len;
+	char	*remainder;
+
+	nl_pos = 0;
+	while (text[nl_pos] && text[nl_pos] != '\n')
+		nl_pos++;
+	if (text[nl_pos] == '\0')
+		return (NULL);
+	rem_len = ft_strlen(text) - nl_pos;
+	if (rem_len <= 1)
+		return (ft_free(text, NULL));
+	remainder = ft_calloc(rem_len + 1, sizeof(char));
+	if (!remainder)
+		return (ft_free(remainder, text));
+	ft_strlcpy(remainder, text + nl_pos + 1, rem_len + 1);
+	free(text);
+	return (remainder);
+}
+
 char	*ft_parse_line(char *line, int fd)
 {
 	char	*buff;
@@ -41,28 +72,28 @@ char	*ft_parse_line(char *line, int fd)
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		bytes_read = read(fd, buff, BUFFER_SIZE);
 		if (bytes_read < 0)
 			return (ft_free(buff, line));
 		buff[bytes_read] = '\0';
 		line = ft_strjoin(line, buff);
 		if (ft_strchr(buff, '\n'))
-			break;
+			break ;
 	}
 	if (bytes_read == 0 && (line[0] == '\0' || !line))
-		return ft_free(line, buff);
+		return (ft_free(line, buff));
 	ft_free(buff, NULL);
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
+	char		*line;
 	static char	*text;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (ft_free(line, NULL));
-	text = ft_get_line(line, fd);
+		return (ft_free(text, NULL));
+	text = ft_parse_line(text, fd);
 	if (!text)
 		return (NULL);
 	line = ft_get_line(text);
