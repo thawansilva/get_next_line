281645a88ed7c39443144fd6418e3b06ccd65cb7
thawansilva <twnpsilva@gmail.com>
2025-07-31 17:20:57 -0300

    Read: Change variables and functions name

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -19,86 +19,86 @@ static void	*ft_free(char *s1, char *s2)
 	return (NULL);
 }
 
-static char	*ft_get_line(char *text)
+static char	*ft_get_line(char *str)
 {
 	size_t	line_len;
 	char	*line;
 
 	line = NULL;
 	line_len = 0;
-	while (text[line_len] && text[line_len] != '\n')
+	while (str[line_len] && str[line_len] != '\n')
 		line_len++;
-	if (!text[line_len])
-		return (text);
-	if (text[line_len] == '\n')
+	if (!str[line_len])
+		return (str);
+	if (str[line_len] == '\n')
 		line_len++;
 	line = ft_calloc(line_len + 1, sizeof(char));
 	if (!line)
 		return (ft_free(line, NULL));
-	ft_strlcpy(line, text, line_len + 1);
+	ft_strlcpy(line, str, line_len + 1);
 	return (line);
 }
 
-static char	*ft_get_remainder(char *text)
+static char	*ft_get_remainder(char *str)
 {
 	size_t	nl_pos;
 	size_t	rem_len;
 	char	*remainder;
 
 	nl_pos = 0;
-	while (text[nl_pos] && text[nl_pos] != '\n')
+	while (str[nl_pos] && str[nl_pos] != '\n')
 		nl_pos++;
-	if (text[nl_pos] == '\0')
+	if (str[nl_pos] == '\0')
 		return (NULL);
-	rem_len = ft_strlen(text) - nl_pos;
+	rem_len = ft_strlen(str) - nl_pos;
 	if (rem_len <= 1)
-		return (ft_free(text, NULL));
+		return (ft_free(str, NULL));
 	remainder = ft_calloc(rem_len + 1, sizeof(char));
 	if (!remainder)
-		return (ft_free(remainder, text));
-	ft_strlcpy(remainder, text + nl_pos + 1, rem_len + 1);
-	free(text);
+		return (ft_free(remainder, str));
+	ft_strlcpy(remainder, str + nl_pos + 1, rem_len + 1);
+	free(str);
 	return (remainder);
 }
 
-static char	*ft_parse_line(char *line, int fd)
+static char	*ft_read_str(char *str, int fd)
 {
 	char	*buff;
 	int		bytes_read;
 
 	buff = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!buff)
-		return (ft_free(buff, line));
+		return (ft_free(buff, str));
 	bytes_read = 1;
 	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
 		if (bytes_read < 0)
-			return (ft_free(buff, line));
+			return (ft_free(buff, str));
 		buff[bytes_read] = '\0';
-		line = ft_strjoin(line, buff);
+		str = ft_strjoin(str, buff);
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
-	if (bytes_read == 0 && (line[0] == '\0' || !line))
-		return (ft_free(line, buff));
+	if (bytes_read == 0 && (str[0] == '\0' || !str))
+		return (ft_free(str, buff));
 	ft_free(buff, NULL);
-	return (line);
+	return (str);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*line;
-	static char	*text[OPEN_MAX];
+	static char	*remainder[OPEN_MAX];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (ft_free(text[fd], NULL));
-	text[fd] = ft_parse_line(text[fd], fd);
-	if (!text[fd])
+		return (ft_free(remainder[fd], NULL));
+	remainder[fd] = ft_read_str(remainder[fd], fd);
+	if (!remainder[fd])
 		return (NULL);
-	line = ft_get_line(text[fd]);
+	line = ft_get_line(remainder[fd]);
 	if (!line)
-		return (ft_free(line, text[fd]));
-	text[fd] = ft_get_remainder(text[fd]);
+		return (ft_free(line, remainder[fd]));
+	remainder[fd] = ft_get_remainder(remainder[fd]);
 	return (line);
 }
